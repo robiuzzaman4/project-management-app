f41b0fe840820a76861add6c7e49e96d162bbf09
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-05-01 10:45:43 +0600

    [DONE] initial project setup done

---
+++ src/providers/react-query-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
